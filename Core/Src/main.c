e6610dbdc8444427ebc873617572822a8e662183
Mikalai Kavalchuk <mikalai.kavalchuk@gmail.com>
2021-09-19 02:46:37 +0300

    Added mass erasing function. It should work from RAM

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,7 +36,7 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+#define RAMFUNC __attribute__ ((section (".RamFunc")))
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -59,6 +59,50 @@ static void MX_USART1_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+
+/**
+ * @brief Mass erasing of FLASH from RAM
+ */
+RAMFUNC void mass_erase_from_ram(void)
+{
+    /* Authorize the FLASH Registers access */
+    WRITE_REG(FLASH->KEYR, FLASH_KEY1);
+    WRITE_REG(FLASH->KEYR, FLASH_KEY2);
+
+    /* Wait for last operation to be completed */
+    while(__HAL_FLASH_GET_FLAG(FLASH_FLAG_BSY))
+    {
+    }
+
+    /* Disable instruction cache  */
+     __HAL_FLASH_INSTRUCTION_CACHE_DISABLE();
+
+     /* Disable data cache  */
+    __HAL_FLASH_DATA_CACHE_DISABLE();
+
+    /* Set the Mass Erase Bit for the bank 1 */
+    SET_BIT(FLASH->CR, FLASH_CR_MER1);
+
+    /* Set the Mass Erase Bit for the bank 2 */
+    SET_BIT(FLASH->CR, FLASH_CR_MER2);
+
+    /* Proceed to erase all sectors */
+    SET_BIT(FLASH->CR, FLASH_CR_STRT);
+
+    /* Wait for last operation to be completed */
+    while(__HAL_FLASH_GET_FLAG(FLASH_FLAG_BSY))
+    {
+    }
+
+    /* Set the LOCK Bit to lock the FLASH Registers access */
+    SET_BIT(FLASH->CR, FLASH_CR_LOCK);
+
+    while(1)
+    {
+
+    }
+}
 
 /* USER CODE END 0 */
 
