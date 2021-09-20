c2985720399baccde4c3e392b87c190ce4a082f3
Mikalai Kavalchuk <mikalai.kavalchuk@gmail.com>
2021-09-20 22:51:59 +0300

    Added custom implementations fof __io_putchar() and __io_getchar()

--- Core/Src/uart_api.c
+++ Core/Src/uart_api.c
@@ -1,12 +1,61 @@
 #include "uart_api.h"
 #include "stm32l4xx_hal.h"
 
+extern UART_HandleTypeDef huart1;
 
+/**
+ * @brief Custom implementation of WEAK __io_putchar() function from syscallc.c
+ */
+int __io_putchar(int ch)
+{
+    if(HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 100) != HAL_OK)
+    {
+        return -1;
+    }
+    return ch;
+}
+
+/**
+ * @brief Custom implementation of WEAK __io_getchar() function from syscallc.c
+ */
+int __io_getchar(void)
+{
+    char data[4];
+    uint8_t ch, len = 1;
+
+    while(HAL_UART_Receive(&huart1, &ch, 1, 0x10) != HAL_OK)
+    {
+        __asm__("nop");
+    }
+
+    memset(data, 0x00, 4);
+    switch(ch)
+    {
+        case '\r':
+        case '\n':
+            len = 2;
+            sprintf(data, "\r\n");
+            break;
+
+        case '\b':
+        case 0x7F:
+            len = 3;
+            sprintf(data, "\b \b");
+            break;
+
+        default:
+            data[0] = ch;
+            break;
+    }
+
+    HAL_UART_Transmit(&huart1, (uint8_t *)&data, len, 100);
+    return ch;
+}
 
 /**
  * @brief Mass erasing of FLASH from RAM
  */
-RAMFUNC void mass_erase_from_ram(void)
+__RAM_FUNC void mass_erase_from_ram(void)
 {
     /* Authorize the FLASH Registers access */
     WRITE_REG(FLASH->KEYR, FLASH_KEY1);
@@ -44,7 +93,10 @@ RAMFUNC void mass_erase_from_ram(void)
 
     while(1)
     {
-
+        __asm__("nop");
     }
 }
+
+
+
 
